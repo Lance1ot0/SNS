f7aae91802cb945968aec1d70f5d22531f81e500
Thierry Maignan <maignan.thierry@outlook.fr>
2022-05-12 23:15:46 +0200

    feat: Navbar html (no css cuz tailwind don't work)

---
+++ shared/Navbar.php
@@ -0,0 +1,30 @@
+<div class="h-16 w-full bg-blue-100">
+  <div id="input-search">
+    <input type="text" />
+  </div>
+
+  <div id="navbar_buttons_container">
+    <ul id="buttons_container">
+      <li>
+        <img src="../images/home.svg" alt="" />
+        <a href="">Home</a>
+      </li>
+      <li>
+        <img src="../images/relation.svg" alt="" />
+        <a href="">Relations</a>
+      </li>
+      <li>
+        <img src="../images/page.svg" alt="" />
+        <a href="">Pages</a>
+      </li>
+      <li>
+        <img src="../images/group.svg" alt="" />
+        <a href="">Groups</a>
+      </li>
+      <li>
+        <img src="../images/message.svg" alt="" />
+        <a href="">Message</a>
+      </li>
+    </ul>
+  </div>
+</div>
