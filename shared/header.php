d5a971ae922c3e92fca1501f89a988d778edfcda
Thierry Maignan <maignan.thierry@outlook.fr>
2022-05-14 21:49:46 +0200

    fix: navbar css

--- shared/header.php
+++ shared/header.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../dist/global.css">
   <title>SNS</title>
 </head>
+
 <body>
-  <nav class="h-16 w-full bg-white flex place-items-center gap-14">
+  <nav class="h-20 w-full bg-white flex place-items-center">
     <div id="input-search">
-      <input type="text" class="w-full placeholder:text-slate-400 border border-blue-300 rounded-md focus:outline-none focus:border-blue-500" placeholder="Search"/>
+      <input type="text" class="w-full placeholder:text-slate-400 border border-blue-300 rounded-md focus:outline-none focus:border-blue-500" placeholder="Search" />
     </div>
 
     <div id="navbar_buttons_container">
-      <ul id="buttons_container" class="flex space-x-10">
-        <li class="flex">
+      <ul id="buttons_container" class="flex gap-10">
+        <li class="flex gap-3">
           <img src="../images/home.svg" alt="" />
           <a href="">Home</a>
         </li>
-        <li class="flex">
+        <li class="flex gap-3">
           <img src="../images/relation.svg" alt="" />
           <a href="">Relations</a>
         </li>
-        <li class="flex">
+        <li class="flex gap-3">
           <img src="../images/page.svg" alt="" />
           <a href="">Pages</a>
         </li>
-        <li class="flex">
+        <li class="flex gap-3">
           <img src="../images/group.svg" alt="" />
           <a href="">Groups</a>
         </li>
-        <li class="flex">
+        <li class="flex gap-3">
           <img src="../images/message.svg" alt="" />
           <a href="">Message</a>
         </li>
       </ul>
     </div>
-    <div></div>
   </nav>
-  
