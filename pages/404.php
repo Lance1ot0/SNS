69b1399ef36633765bdbcba5d8b11b1e10099128
Thierry Maignan <maignan.thierry@outlook.fr>
2022-05-17 10:01:06 +0200

    feat: notFound page

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,7 @@
-404 not found
+<div class="w-full h-full bg-blue-100 flex justify-center items-center">
+    <div class="flex justify-center items-center flex-col gap-10">
+        <h1 class="text-blue-900 text-9xl font-black">404</h1>
+        <p class="text-blue-900 font-medium text-lg">Well, this is awkward. There's nothing so see here</p>
+        <button class="button flex justify-center items-center">Back to homepage</button>
+    </div>
+</div>
