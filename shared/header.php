95fc4a174286ae8fe5e988db220491112254574a
Nangaim <maignan.thierry@outlook.fr>
2022-05-19 21:21:18 +0200

    feat: search bar no css

--- shared/header.php
+++ shared/header.php
@@ -12,6 +12,26 @@ if (!isset($_SESSION['user'])) {
   $user_data = $_SESSION['user'];
 }
 
+$database = new Database();
+$db = $database->connect();
+
+$user = new User($db); 
+
+$users = $user->get_all();
+$users = json_decode($users);
+$data_users = array();
+
+for ($i = 0; $i < count($users); $i++){
+  $data_users[] = array(
+    "id" => $users[$i]->id,
+    "firstname" => $users[$i]->firstname,
+    "lastname" => $users[$i]->lastname
+  );
+}
+
+$data_users = json_encode($data_users);
+
+file_put_contents('allUsers.json', $data_users);
 ?>
 
 <!DOCTYPE html>
@@ -33,9 +53,18 @@ if (!isset($_SESSION['user'])) {
     </a>
 
     <div class="max-w-md">
-      <input type="text"
-        class="px-4 py-2 border-2 transition-[border-color] duration-300 focus:border-blue-500 border-blue-300 outline-none rounded-md text-lg placeholder:text-blue-300 text-blue-500"
-        placeholder="Search" />
+      <form action="" method="POST">
+        
+        <input type="text"
+          class="px-4 py-2 border-2 transition-[border-color] duration-300 focus:border-blue-500 border-blue-300 outline-none rounded-md text-lg placeholder:text-blue-300 text-blue-500"
+          placeholder="Search"
+          name="searchUsers" 
+          id="searchUsers"
+          autocomplete="off"
+           />
+
+          </form>
+          <div class="match-list"></div>
     </div>
 
     <div class="mr-24 text-lg">
@@ -60,4 +89,54 @@ if (!isset($_SESSION['user'])) {
       </ul>
     </div>
   </nav>
+  <script>
+    const search = document.querySelector('#searchUsers');
+
+    const matchList = document.querySelector('.match-list');
+
+
+
+    const searchStates = async searchText => {
+      const res = await fetch('allUsers.json');
+      const states = await res.json();
+
+      const matches = states.filter(state => {
+          const regex = new RegExp(`^${searchText}`, 'gi');
+          
+          return toString(state.id).match(regex) || state.firstname.match(regex) || state.lastname.match(regex);
+      });
+
+
+      if (searchText === 0) {
+          matches = [];
+          
+
+      };
+      outputHtml(matches);
+
+    };
+
+    
+    const outputHtml = matches => {
+
+      if (search.value === ""){
+        matchList.innerHTML = ""
+      }else if (matches.length > 0) {
+          const html = matches.map(match => `
+              <ul> 
+                <li>  
+                  <a href="/profile?u=${match.id}" class="linkPage"> ${match.firstname} ${match.lastname} </a> 
+                </li>
+              </ul>
+              `).join('');
+
+          matchList.innerHTML = html;
+      } else {
+          matchList.innerHTML = '<p class="noMatch">Aucune Correspondance</p>'
+      }
+    };
+
+    search.addEventListener('input', () => searchStates(search.value))
+  </script>
   <?php endif ?>
+
